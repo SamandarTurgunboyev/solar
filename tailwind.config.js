5098ce312acb7d091c0da55443e10b072fe25cb0
Usmonov Azizbek <usmonovazizbek045@gmail.com>
2023-08-31 19:03:23 +0500

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,21 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      barlowBlack: 'barlow-black, sans-serif',
+      barlowExtraBold: 'barlow-extrabold, sans-serif',
+      barlowBold: 'barlow-bold, sans-serif',
+      barlowSemiBold: 'barlow-semibold, sans-serif',
+      barlowMedium: 'barlow-medium, sans-serif',
+      barlowRegular: 'barlow-regular, sans-serif',
+      tahomaRegular: 'tahoma-regular, sans-serif'
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        blue: '#273968',
+        red: '#dc3545',
+        yellow: '#FFCD1E'
+      }
     },
   },
   plugins: [],

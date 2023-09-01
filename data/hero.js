e0cc9f67bda3b71eadf1447733c60098260afc63
Samandar Turg'unboev <123394292+SamandarTurgunboyev@users.noreply.github.com>
2023-09-01 20:55:03 +0500

    hero

---
+++ data/hero.js
@@ -0,0 +1,39 @@
+const hero = [
+    {
+        id: 1,
+        title: "hero.page_1.title",
+        link_1: 'hero.link_1',
+        linkl_2: "hero.link_2",
+        icon: '../images/hero1.jpg'
+    },
+    {
+        id: 2,
+        title: "hero.page_2.title",
+        link_1: 'hero.link_1',
+        linkl_2: "hero.link_2",
+        icon: '.../images/hero2.jpg'
+    },
+    {
+        id: 3,
+        title: "hero.page_3.title",
+        link_1: 'hero.link_1',
+        linkl_2: "hero.link_2",
+        icon: '.../images/hero3.jpg'
+    },
+    {
+        id: 4,
+        title: "hero.page_4.title",
+        link_1: 'hero.link_1',
+        linkl_2: "hero.link_2",
+        icon: '.../images/hero4.jpg'
+    },
+    {
+        id: 5,
+        title: "hero.page_5.title",
+        link_1: 'hero.link_1',
+        linkl_2: "hero.link_2",
+        icon: '.../images/hero5.jpg'
+    },
+]
+
+export default hero
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { Offers, Statistics } from '@/components'
+import Hero from '@/components/Hero'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
   return (
     <div>
       <Offers />
       <Statistics />
+      <Hero />
     </div>
+
   )
 }
 
